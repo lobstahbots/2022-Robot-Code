3c7823e0b9dabb98f496ef8c7e7f57ca43efa188
Aidan Mundy <amboyscout@aol.com>
2022-01-13 20:14:03 -0500

    Add documentation for some undocumented classes

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -7,6 +7,9 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+/**
+ * A subsystem that controls the drive train (aka chassis) on a robot
+ */
 public class DriveBase extends SubsystemBase {
 
   private final CANSparkMax leftFrontMotor;
@@ -16,10 +19,27 @@ public class DriveBase extends SubsystemBase {
 
   private final DifferentialDrive differentialDrive;
 
+  /**
+   * Constructs a DriveBase with a {@link CANSparkMax} at each of the given CAN IDs
+   * 
+   * @param leftFrontID The CAN ID of the Left Front motor
+   * @param leftBackID The CAN ID of the Left Back motor
+   * @param rightFrontID The CAN ID of the Right Front motor
+   * @param rightBackID The CAN ID of the Right Back motor
+   */
   public DriveBase(int leftFrontID, int leftBackID, int rightFrontID, int rightBackID) {
     this(leftFrontID, leftBackID, rightFrontID, rightBackID, MotorType.kBrushless);
   }
 
+  /**
+   * Constructs a DriveBase with a {@link CANSparkMax} at each of the given CAN IDs
+   * 
+   * @param leftFrontID The CAN ID of the Left Front motor
+   * @param leftBackID The CAN ID of the Left Back motor
+   * @param rightFrontID The CAN ID of the Right Front motor
+   * @param rightBackID The CAN ID of the Right Back motor
+   * @param motorType The {@link MotorType} of the motors attached to the {@link CANSparkMax}es
+   */
   public DriveBase(int leftFrontID, int leftBackID, int rightFrontID, int rightBackID, MotorType motorType) {
     leftFrontMotor = new CANSparkMax(leftFrontID, motorType);
     leftBackMotor = new CANSparkMax(leftBackID, motorType);
@@ -32,6 +52,9 @@ public class DriveBase extends SubsystemBase {
     );
   }
 
+  /**
+   * Toggles the {@link IdleMode} between Coast and Brake
+   */
   public void toggleBrakingMode() {
     switch (leftFrontMotor.getIdleMode()) {
       case kBrake:
@@ -44,6 +67,11 @@ public class DriveBase extends SubsystemBase {
     }
   }
 
+  /**
+   * Sets the braking mode to the given {@link IdleMode}
+   * 
+   * @param mode
+   */
   public void setBrakingMode(IdleMode mode) {
     leftFrontMotor.setIdleMode(mode);
     leftBackMotor.setIdleMode(mode);
