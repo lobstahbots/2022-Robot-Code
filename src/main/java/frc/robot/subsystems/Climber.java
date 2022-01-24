8bca0f94f489ff87a47be6271f7d12619c57ecd0
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2022-01-23 20:56:02 -0500

    Create Climber Subsystem

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,22 @@
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase {
+  private final CANSparkMax climberMotor;
+
+  public Climber(int motorId, MotorType motorType) {
+    climberMotor = new CANSparkMax(motorId, motorType);
+    climberMotor.setIdleMode(IdleMode.kBrake);
+  }
+
+  public void setSpeed(double speed) {
+    climberMotor.set(speed);
+  }
+
+}
