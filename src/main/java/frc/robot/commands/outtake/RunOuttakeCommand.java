cddda63df3fd64199e56d702fec727c1e72e2892
chit-sudeep <79122331+chit-sudeep@users.noreply.github.com>
2022-01-24 21:31:32 -0500

    Create files with commands (no code)

---
+++ src/main/java/frc/robot/commands/outtake/RunOuttakeCommand.java
@@ -0,0 +1,9 @@
+
+package frc.robot.commands.outtake;
+
+import frc.robot.subsystems.Outtake;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class RunOuttakeCommand extends CommandBase {
+
+}
---
+++ src/main/java/frc/robot/commands/outtake/StopOuttakeCommand.java
@@ -0,0 +1,10 @@
+
+package frc.robot.commands.outtake;
+
+import frc.robot.subsystems.Outtake;
+import java.util.function.Supplier;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class StopOuttakeCommand extends CommandBase {
+
+}
