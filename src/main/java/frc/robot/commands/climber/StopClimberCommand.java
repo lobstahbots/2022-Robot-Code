4d0bc3d126fbd9b7a67cc737bf168b2d93575470
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2022-01-25 09:27:42 -0500

    Create StopClimberCommand

---
+++ src/main/java/frc/robot/commands/climber/StopClimberCommand.java
@@ -0,0 +1,36 @@
+
+package frc.robot.commands.climber;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Climber;
+
+public class StopClimberCommand extends CommandBase {
+  private final Climber climber;
+
+  /** Creates a new StopClimberCommand. */
+  public StopClimberCommand(Climber climber) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.climber = climber;
+    addRequirements(this.climber);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    climber.setSpeed(0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
