9d376aa2617bd49cbbc2c45b8aed8e11f5bf1366
ch33zZz <jin.choi9000@gmail.com>
2022-01-24 20:36:34 -0500

    Added descriptions

--- src/main/java/frc/robot/subsystems/Tower.java
+++ src/main/java/frc/robot/subsystems/Tower.java
@@ -10,23 +10,49 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+// Subsystem that controls the tower of the robot
+
 public class Tower extends SubsystemBase {
   /** Creates a new Tower. */
-
   private final CANSparkMax towerMotor;
+
+  /**
+   * Constructs a Tower with a {@link CANSparkMax} at the given CAN ID.
+   *
+   * @param towerMotorId The CAN ID of the Tower motor
+   */
 
   public Tower(int towerMotorId) {
     this(towerMotorId, MotorType.kBrushless);
     setBrakingMode(IdleMode.kBrake);
   }
 
+  /**
+   * Constructs a Tower with a {@link CANSparkMax} at the given CAN ID.
+   *
+   * @param towerMotorId The CAN ID of the Tower motor
+   * @param motorType The {@link MotorType} of the motors attached to the {@link CANSparkMax}
+   */
+
   public Tower(int towerMotorId, MotorType motorType) {
     towerMotor = new CANSparkMax(towerMotorId, motorType);
   }
 
+  /**
+   * Sets the braking mode to the given {@link IdleMode}.
+   *
+   * @param mode The {@link IdleMode} to set the motors to
+   */
+
   public void setBrakingMode(IdleMode mode) {
     towerMotor.setIdleMode(mode);
   }
+
+  /**
+   * Sets the Tower Motor's speed
+   * 
+   * @param speed
+   */
 
   public void setSpeed(double speed) {
     towerMotor.set(speed);
