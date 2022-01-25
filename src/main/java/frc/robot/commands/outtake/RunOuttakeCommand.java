fb786027ec36f62b37f860e83456239fb1ac29b5
chit-sudeep <79122331+chit-sudeep@users.noreply.github.com>
2022-01-24 21:42:14 -0500

    Add javadoc and execute and isFinished methods (empty)

--- src/main/java/frc/robot/commands/outtake/RunOuttakeCommand.java
+++ src/main/java/frc/robot/commands/outtake/RunOuttakeCommand.java
@@ -4,6 +4,18 @@ package frc.robot.commands.outtake;
 import frc.robot.subsystems.Outtake;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/**
+ * Sets outtake speed to a given double.
+ */
 public class RunOuttakeCommand extends CommandBase {
 
+  @Override
+  public void execute() {
+
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
--- src/main/java/frc/robot/commands/outtake/StopOuttakeCommand.java
+++ src/main/java/frc/robot/commands/outtake/StopOuttakeCommand.java
@@ -2,9 +2,20 @@
 package frc.robot.commands.outtake;
 
 import frc.robot.subsystems.Outtake;
-import java.util.function.Supplier;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/**
+ * Repeatedly sets the outtake speed to 0.
+ */
 public class StopOuttakeCommand extends CommandBase {
 
+  @Override
+  public void execute() {
+
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
