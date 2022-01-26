8dce2070ba067fc4e56bc5e6556ade05fb01d31d
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2022-01-25 22:03:52 -0500

    Create Intake commands

---
+++ src/main/java/frc/robot/commands/intake/RetractIntakeCommand.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Intake;
+
+/**
+ * Retracts the given {@link Intake}.
+ */
+public class RetractIntakeCommand extends InstantCommand {
+
+  /**
+   * Creates an instantly run command that retracts the given {@link Intake}.
+   * 
+   * @param intake The {@link Intake} to control.
+   */
+  public RetractIntakeCommand(Intake intake) {
+    super(() -> {
+      intake.setRetracted();
+    }, intake);
+  }
+}
