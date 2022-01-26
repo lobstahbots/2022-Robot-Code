a79c23fdf8902b2adbda79ff6424a7cf28df0f2a
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2022-01-25 21:14:37 -0500

    Write basic commands for Intake

---
+++ src/main/java/frc/robot/commands/intake/ToggleExtensionIntakeCommand.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Intake;
+
+/**
+ * Toggles the {@link DoubleSolenoid.Value} (aka extension/retraction) of the given {@link Intake}.
+ */
+public class ToggleExtensionIntakeCommand extends InstantCommand {
+
+  /**
+   * Creates a command that toggles the {@link DoubleSolenoid.Value} (aka extension/retraction) of
+   * the given {@link Intake}.
+   * 
+   * @param intake The {@link Intake} to control.
+   */
+  public ToggleExtensionIntakeCommand(Intake intake) {
+    super(() -> {
+      intake.toggle();
+    }, intake);
+  }
+}
