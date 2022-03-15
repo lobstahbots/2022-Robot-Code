71e75cee96caccef08adb5c79b66fb6e4511d481
chit-sudeep <79122331+chit-sudeep@users.noreply.github.com>
2022-03-14 23:29:59 -0400

    Add Position Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,13 +5,13 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
+ * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
  * <p>
- * It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * It is advised to statically import this class (or one of its inner classes) wherever the constants are needed, to
+ * reduce verbosity.
  */
 public final class Constants {
   public static final double SIMPLE_AUTON_SPEED = 0.5; // WEEK0
@@ -104,6 +104,9 @@ public final class Constants {
     public static final int CLIMBER_CURRENT_LIMIT = 85;
     public static final int CLIMBER_TRIGGER_THRESHOLD = 100;
     public static final double CLIMBER_TRIGGER_THRESHOLD_TIME = 0.5;
+
+    public static final int CLIMBER_RETRACTED_POSITION = 0;
+    public static final int CLIMBER_EXTENDED_POSITION = 2048; // PLACEHOLDER
   }
 
   /**
