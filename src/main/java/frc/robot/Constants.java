0782b4a3818658354417f51d02722702406afc24
Aidan Mundy <amboyscout@aol.com>
2022-04-09 02:05:34 -0400

    Remove old comments from Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,9 +19,9 @@ public final class Constants {
    * Stores constants related to autonomous routines.
    */
   public static final class AutonConstants {
-    public static final double SIMPLE_AUTON_SPEED = 0.7; // WEEK0
-    public static final double SIMPLE_AUTON_RUNTIME = 3.0; // WEEK0
-    public static final double MEDIUM_AUTON_OUTTAKE_RUNTIME = 3.0; // PLACEHOLDER
+    public static final double SIMPLE_AUTON_SPEED = 0.7;
+    public static final double SIMPLE_AUTON_RUNTIME = 3.0;
+    public static final double MEDIUM_AUTON_OUTTAKE_RUNTIME = 3.0;
   }
 
   /**
@@ -31,13 +31,13 @@ public final class Constants {
     public static final int DRIVER_JOYSTICK_INDEX = 0;
 
     public static final class DriverButtons {
-      public static final int SLOWDOWN1 = 6; // PLACEHOLDER
-      public static final int SLOWDOWN2 = 5; // PLACEHOLDER
+      public static final int SLOWDOWN1 = 6;
+      public static final int SLOWDOWN2 = 5;
     }
 
     public static final class DriverAxes {
-      public static final int LEFT = 1; // PLACEHOLDER
-      public static final int RIGHT = 5; // PLACEHOLDER
+      public static final int LEFT = 1;
+      public static final int RIGHT = 5;
     }
 
     public static final int OPERATOR_JOYSTICK_INDEX = 1;
@@ -45,12 +45,12 @@ public final class Constants {
     public static final class OperatorButtons {
       public static final int INTAKE = 3; // PLACEHOLDER
 
-      public static final int OUTTAKE = 1; // WEEK0
+      public static final int OUTTAKE = 1;
 
-      public static final int TOWER = 2; // WEEK0
+      public static final int TOWER = 2; // PLACEHOLDER
 
-      public static final int CLIMBER_UP = 6; // WEEK0
-      public static final int CLIMBER_DOWN = 5; // WEEK0
+      public static final int CLIMBER_UP = 6;
+      public static final int CLIMBER_DOWN = 5;
       public static final int CLIMBER_RETRACT = 11;
       public static final int CLIMBER_EXTEND = 12;
     }
@@ -66,10 +66,10 @@ public final class Constants {
     public static final double ACCELERATION_RATE_LIMIT = 2.1;
 
     public static final class DriveMotorCANIDs {
-      public static final int LEFT_FRONT = 42; // WEEK0
-      public static final int LEFT_BACK = 41; // WEEK0
-      public static final int RIGHT_FRONT = 44; // WEEK0
-      public static final int RIGHT_BACK = 43; // WEEK0
+      public static final int LEFT_FRONT = 42;
+      public static final int LEFT_BACK = 41;
+      public static final int RIGHT_FRONT = 44;
+      public static final int RIGHT_BACK = 43;
     }
 
     public static final int CURRENT_LIMIT = 60;
@@ -94,20 +94,20 @@ public final class Constants {
 
     public static final int MOTOR_ID = 0; // PLACEHOLDER
 
-    public static final int CURRENT_LIMIT = 40;
-    public static final int TRIGGER_THRESHOLD = 40;
-    public static final int TRIGGER_THRESHOLD_TIME = 2;
+    public static final int CURRENT_LIMIT = 40; // PLACEHOLDER
+    public static final int TRIGGER_THRESHOLD = 40; // PLACEHOLDER
+    public static final int TRIGGER_THRESHOLD_TIME = 2; // PLACEHOLDER
   }
 
   /**
    * Stores constants related to the Outtake.
    */
   public static final class OuttakeConstants {
-    public static final double SPEED = 0.4; // PLACEHOLDER
+    public static final double SPEED = 0.4;
 
     public static final class OuttakeMotorCANIDs {
-      public static final int TOP = 62; // PLACEHOLDER
-      public static final int BOTTOM = 61; // PLACEHOLDER
+      public static final int TOP = 62;
+      public static final int BOTTOM = 61;
     }
 
     public static final int CURRENT_LIMIT = 40;
@@ -126,18 +126,18 @@ public final class Constants {
       public static final int BOTTOM_RIGHT = 54; // PLACEHOLDER
     }
 
-    public static final int CURRENT_LIMIT = 25;
+    public static final int CURRENT_LIMIT = 25; // PLACEHOLDER
   }
 
   /**
    * Stores constants related to the Climber.
    */
   public static final class ClimberConstants {
-    public static final double SPEED = 1.0; // WEEK0
+    public static final double SPEED = 1.0;
 
     public static final class ClimberMotorCANIDs {
-      public static final int LEFT = 31; // PLACEHOLDER
-      public static final int RIGHT = 32; // PLACEHOLDER
+      public static final int LEFT = 31;
+      public static final int RIGHT = 32;
     }
 
     public static final int CURRENT_LIMIT = 85;
