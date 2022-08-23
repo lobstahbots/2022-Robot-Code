74eaef5e6ef1eeef06b9dd3206151aa5bc1cbd52
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2022-08-23 19:24:40 -0400

    Finish simple vision motion controller

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -43,9 +43,11 @@ public final class Constants {
 
   public static final class LimelightConstants {
     public static final double TURN_Kp = 0.1;
+    public static final double MIN_TURN_SPEED = 0.1;
+    public static final double MAX_TURN_SPEED = 0.1;
     public static final double TURN_ERROR_THRESHOLD = 1;
     public static final double DRIVE_SPEED = 0.7;
-    public static final double AREA_THRESHOLD =
+    public static final double AREA_THRESHOLD = 0.8;
   }
 
   /**
--- src/main/java/frc/robot/commands/drive/VisionMotionControllerCommand.java
+++ src/main/java/frc/robot/commands/drive/VisionMotionControllerCommand.java
@@ -6,6 +6,7 @@ package frc.robot.commands.drive;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.LimelightConstants;
 import frc.robot.subsystems.DriveBase;
 import frc.robot.subsystems.Limelight;
@@ -13,6 +14,7 @@ import frc.robot.subsystems.Limelight;
 public class VisionMotionControllerCommand extends CommandBase {
   private final Limelight limelight;
   private final DriveBase driveBase;
+  private CommandBase currentCommand;
 
   /** Creates a new CheckTurnError. */
   public VisionMotionControllerCommand(DriveBase driveBase, Limelight limelight) {
@@ -22,16 +24,25 @@ public class VisionMotionControllerCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    currentCommand = new VisionDriveCommand(driveBase, limelight);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (Math.abs(limelight.getTx()) > LimelightConstants.TURN_ERROR_THRESHOLD) {
-      CommandScheduler.getInstance().schedule(
-          new VisionTurnCommand(driveBase, limelight),
-          new VisionDriveCommand(driveBase, limelight));
+    if (Math.abs(limelight.getTx()) > LimelightConstants.TURN_ERROR_THRESHOLD
+      && currentCommand instanceof VisionDriveCommand
+    ) {
+      currentCommand = new VisionTurnCommand(driveBase, limelight);
+      CommandScheduler.getInstance().schedule(currentCommand);
     }
+
+    if (currentCommand instanceof VisionTurnCommand
+      && !CommandScheduler.getInstance().isScheduled(currentCommand)) {
+        currentCommand = new VisionDriveCommand(driveBase, limelight);
+        CommandScheduler.getInstance().schedule(currentCommand);
+      }
   }
 
   // Called once the command ends or is interrupted.
@@ -41,7 +52,7 @@ public class VisionMotionControllerCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return CommandScheduler.getInstance().isScheduled(new VisionTurnCommand(driveBase, limelight),
-        new VisionDriveCommand(driveBase, limelight));
+    return !CommandScheduler.getInstance().isScheduled(currentCommand)
+      && currentCommand instanceof VisionDriveCommand;
   }
 }
